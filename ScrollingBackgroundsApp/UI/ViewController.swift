471f1c3088c447a036eb6790ba8923b1eaaac919
Dariusz Rybicki <darrarski@gmail.com>
2017-06-07 14:31:25 +0200

    Add Xcode project with iOS app target

---
+++ ScrollingBackgroundsApp/UI/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .white
+    }
+
+}
